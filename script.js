4ee399cd1386238db7c272048e0d7d78493fc9e8
Maksim Yavorets | Front-End Developer <styn1sss@gmail.com>
2024-08-31 07:55:40 +0300

    Finished BEM

--- script.js
+++ script.js
@@ -2,40 +2,55 @@ const burgerBtn = document.querySelector(".header__burger-btn");
 const exitBtn = document.querySelector(".header__exit-btn");
 const mobileMenu = document.querySelector(".header__mobile-menu");
 
-function showMobileMenu(){
-    burgerBtn.style.display = "none";
-    exitBtn.style.display = "block";
-    mobileMenu.style.display = "block";
+function showMobileMenu() {
+  burgerBtn.style.display = "none";
+  exitBtn.style.display = "block";
+  mobileMenu.style.display = "block";
 }
 
-function hideMobileMenu(){
-    burgerBtn.style.display = "block";
-    exitBtn.style.display = "none";
-    mobileMenu.style.display = "none";
+function hideMobileMenu() {
+  burgerBtn.style.display = "block";
+  exitBtn.style.display = "none";
+  mobileMenu.style.display = "none";
 }
 
-function hideAll(){
-    burgerBtn.style.display = "none";
-    exitBtn.style.display = "none";
-    mobileMenu.style.display = "none";
+function hideAll() {
+  burgerBtn.style.display = "none";
+  exitBtn.style.display = "none";
+  mobileMenu.style.display = "none";
 }
 
-function updateView(){
-    var windowWidth = window.innerWidth;
-    if (windowWidth > 768){
-        hideAll();
+function updateView() {
+  var windowWidth = window.innerWidth;
+  if (windowWidth > 768) {
+    hideAll();
+    returnImage();
+  } else {
+    if (exitBtn.style.display === "none") {
+      burgerBtn.style.display = "block";
     }
-    else{
-        if (exitBtn.style.display === "none"){
-            burgerBtn.style.display = "block";
-        }
-    }
-    
+    replaceImage();
+  }
 }
 
-document.addEventListener("DOMContentLoaded", function(){
-    burgerBtn.addEventListener("click", showMobileMenu);
-    exitBtn.addEventListener("click", hideMobileMenu);
-    window.addEventListener("resize", updateView);
-    updateView();
+function replaceImage() {
+  const familyPic = document.querySelector(".advantages__family-pic");
+  const header = document.querySelector("header");
+  const main = document.querySelector("main");
+
+  header.parentNode.insertBefore(familyPic, main);
+}
+
+function returnImage() {
+  const familyPic = document.querySelector(".advantages__family-pic");
+  const advantagesSection = document.querySelector('.advantages');
+
+  advantagesSection.prepend(familyPic);
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+  burgerBtn.addEventListener("click", showMobileMenu);
+  exitBtn.addEventListener("click", hideMobileMenu);
+  window.addEventListener("resize", updateView);
+  updateView();
 });
